174fd4b0a566aaca30f84bf10eb88a7e422c4e68
KrashArh <152344332+KrashArh@users.noreply.github.com>
2023-11-29 11:53:19 +0300

    Add files via upload

---
+++ Calculator/src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String name = "Calculator_Java_test";
+        System.out.println(name);
+
+        int a;
+        int b;
+        int result;
+
+        a = 1;
+        b = 2;
+        result = a + b;
+        System.out.println(result);
+
+        a = 6;
+        b = 3;
+        result = a / b;
+        System.out.println(result);
+
+        a = 1;
+        b = 2;
+        result = a - b;
+        System.out.println(result);
+
+        a = 1;
+        b = 1;
+        result = a + b;
+        System.out.println(result);
+
+        a = 1;
+        result = a;
+        System.out.println(result);
+
+        a = 1;
+        b = 2;
+        result = a + b +3;
+        System.out.println(result);
+
+
+    }
+}
